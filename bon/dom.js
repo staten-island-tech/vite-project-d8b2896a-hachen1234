be7ee478e62d3189e9bd2a57ae8b4af1929bae9b
hachen1234 <90781150+hachen1234@users.noreply.github.com>
2023-11-06 10:17:04 -0500

    11

--- bon/dom.js
+++ bon/dom.js
@@ -12,7 +12,7 @@ const data = [
     Image: "bon/img/img2.jpg",
   },
   {
-    name: "Hittime Fun Runny Nose Shower Gel Dispenser ",
+    name: "Hittime Fun Runny Nose Shower Gel Dispenser",
     cost: "Currently unavailable",
     link: "https://www.amazon.com/Runny-Nose-Soap-Shower-Dispenser/dp/B004E4EQOY/?tag=zdgeek-20",
     Image: "bon/img/img4.jpg",
@@ -22,6 +22,42 @@ const data = [
     cost: "$8.79",
     link: "https://www.amazon.com/How-Talk-Your-About-Safety/dp/045149492X/ref=pd_bxgy_img_sccl_1/140-3930159-9166957?pd_rd_w=yVIHN&content-id=amzn1.sym.43d28dfc-aa4f-4ef6-b591-5ab7095e137f&pf_rd_p=43d28dfc-aa4f-4ef6-b591-5ab7095e137f&pf_rd_r=641AS4Z7PVNEVP1MTCCN&pd_rd_wg=lf1y1&pd_rd_r=1f747ca4-807a-43dc-9533-e2b147ea6cfa&pd_rd_i=045149492X&psc=1",
     Image: "bon/img/img5.jpg",
+  },
+  {
+    name: "Doll Head Ring",
+    cost: "$15.95",
+    link: "https://www.etsy.com/listing/904938871/doll-head-ring-doll-ring-adjustable-ring",
+    Image: "bon/img/img6.jpg",
+  },
+  {
+    name: "Taxidermy Rat Fantasy",
+    cost: "$185.00",
+    link: "https://www.etsy.com/listing/1431730766/taxidermy-rat-fantasy?gpla=1&gao=1&",
+    Image: "bon/img/img7.jpg",
+  },
+  {
+    name: "Canned Unicorn Meat Lunch Spread Gag Gift!",
+    cost: "$6.99",
+    link: "https://www.amazon.com/Canned-Unicorn-Meat-Lunch-Spread/dp/B0781DKT3Z/ref=sr_1_2?amazon-ids-by-cc=US%3DRanker76278-20&keywords=Canned+Unicorn+Meat&qid=1699282912&sr=8-2",
+    Image: "bon/img/img8.jpg",
+  },
+  {
+    name: "Steampunk Winged Guinea Pig Armor",
+    cost: "$74.95 ",
+    link: "https://www.thisiswhyimbroke.com/steampunk-winged-guinea-pig-armor/",
+    Image: "bon/img/img9.jpg",
+  },
+  {
+    name: "Funny Caveman Slippers",
+    cost: "$75.00",
+    link: "https://www.etsy.com/listing/1353260315/funny-caveman-slippers?show_sold_out_detail=1&source=aw&utm_source=affiliate_window&utm_medium=affiliate&utm_campaign=us_location_buyer&utm_content=141392&utm_term=0&sv1=affiliate&sv_campaign_id=141392&awc=6220_1699283379_e961166f2f31101d53dbed27f647f5ae",
+    Image: "bon/img/img10.webp",
+  },
+  {
+    name: "Allosaurus Dinosaur Life Size Statue",
+    cost: "$2,978.08",
+    link: "https://www.etsy.com/listing/1262308352/allosaurus-dinosaur-life-size-statue?click_key=c46308cc38dc66d9e1c5b2fa318a9c54c651e0c4%3A1262308352&click_sum=fe9049af&ref=sold_out-4&frs=1",
+    Image: "bon/img/img11.webp",
   },
 ];
 
