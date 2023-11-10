498eec9f4a057bf6b74789f2591f31d65b6a7d1f
hachen1234 <90781150+hachen1234@users.noreply.github.com>
2023-11-10 10:14:34 -0500

    1

--- bon/js/main.js
+++ bon/js/main.js
@@ -1,3 +1,15 @@
 import "../css/style.css";
 import { data } from "./dom";
-data.forEach((data) => console.log(data));
+data.forEach((data) => {
+  function insertcard() {
+    data.display.insertAdjacentHTML(
+      "afterbegin",
+      `<div class="card">
+            <h2 class="name">${data.name}</h2>
+            <img class="image"src="${data.Image}" alt="">
+            <h3 class="price">${data.cost}</h3>
+            </div>`
+    );
+  }
+  insertcard();
+});
