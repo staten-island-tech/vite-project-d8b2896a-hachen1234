917f319d4218a7dddfd4b8620498145dff7b27a3
hachen1234 <90781150+hachen1234@users.noreply.github.com>
2023-11-17 10:16:57 -0500

    1

--- bon/js/main.js
+++ bon/js/main.js
@@ -17,10 +17,27 @@ data.forEach((data) => {
   insertcard();
 });
 dom.instockbtn.addEventListener("click", function () {
-  e.target.parentElement.remove(insertcard);
   dom.display.innerHTML = "";
   data
     .filter((data) => data.instock === true)
+    .forEach((data) => {
+      dom.display.insertAdjacentHTML(
+        "afterend",
+        ` 
+      <div class="card">
+            <h2 class="name">${data.name}</h2>
+            <img class="image"src="${data.Image}" alt="">
+            <h3 class="price">${data.cost}</h3>
+            <a href="${data.link}"> <button>Click Here to Visit Site</button> </a>
+            </div>
+            `
+      );
+    });
+});
+dom.animalbtn.addEventListener("click", function () {
+  dom.display.innerHTML = "";
+  data
+    .filter((data) => data.animalrelated === true)
     .forEach((data) => {
       dom.display.insertAdjacentHTML(
         "afterend",
