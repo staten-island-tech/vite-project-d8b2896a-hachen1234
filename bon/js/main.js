514ea3df44cbb2040e8658da04615ec578c2a9ec
hachen1234 <90781150+hachen1234@users.noreply.github.com>
2023-11-27 10:14:59 -0500

    1

--- bon/js/main.js
+++ bon/js/main.js
@@ -1,75 +1,91 @@
 import "../css/style.css";
 import { data, dom } from "./dom";
-data.forEach((data) => {
-  function insertcard() {
-    dom.display.insertAdjacentHTML(
-      "afterbegin",
-      ` 
-      <div class="card">
-            <h2 class="name">${data.name}</h2>
-            <img class="image"src="${data.Image}" alt="">
-            <h3 class="price">${data.cost}</h3>
-            <a href="${data.link}"> <button>Click Here to Visit Site</button> </a>
-            </div>
-          
-            `
-    );
-  }
-  insertcard();
-});
-dom.totalbtn.addEventListener("click", function () {
+// data.forEach((data) => {
+//   function insertcard() {
+//     dom.display.insertAdjacentHTML(
+//       "afterbegin",
+//       `
+//       <div class="card">
+//             <h2 class="name">${data.name}</h2>
+//             <img class="image"src="${data.Image}" alt="">
+//             <h3 class="price">${data.cost}</h3>
+//             <a href="${data.link}"> <button>Click Here to Visit Site</button> </a>
+//             </div>
+
+//             `
+//     );
+//   }
+//   insertcard();
+// });
+// dom.totalbtn.addEventListener("click", function () {
+//   dom.display.innerHTML = "";
+//   data.forEach((data) => {
+//     dom.display.insertAdjacentHTML(
+//       "afterbegin",
+//       `
+//     <div class="card">
+//           <h2 class="name">${data.name}</h2>
+//           <img class="image"src="${data.Image}" alt="">
+//           <h3 class="price">${data.cost}</h3>
+//           <a href="${data.link}"> <button>Click Here to Visit Site</button> </a>
+//           </div>
+//           `
+//     );
+//   });
+// });
+// dom.instockbtn.addEventListener("click", function () {
+//   dom.display.innerHTML = "";
+//   data
+//     .filter((data) => data.instock === true)
+//     .forEach((data) => {
+//       dom.display.insertAdjacentHTML(
+//         "afterbegin",
+//         `
+//       <div class="card">
+//             <h2 class="name">${data.name}</h2>
+//             <img class="image"src="${data.Image}" alt="">
+//             <h3 class="price">${data.cost}</h3>
+//             <a href="${data.link}"> <button>Click Here to Visit Site</button> </a>
+//             </div>
+//             `
+//       );
+//     });
+// });
+// dom.animalbtn.addEventListener("click", function () {
+//   dom.display.innerHTML = "";
+//   data
+//     .filter((data) => data.animalrelated === true)
+//     .forEach((data) => {
+//       dom.display.insertAdjacentHTML(
+//         "afterbegin",
+//         `
+//       <div class="card">
+//             <h2 class="name">${data.name}</h2>
+//             <img class="image"src="${data.Image}" alt="">
+//             <h3 class="price">${data.cost}</h3>
+//             <a href="${data.link}"> <button>Click Here to Visit Site</button> </a>
+//             </div>
+//             `
+//       );
+//     });
+// });
+function clear() {
   dom.display.innerHTML = "";
-  data.forEach((data) => {
-    dom.display.insertAdjacentHTML(
-      "afterbegin",
-      ` 
-    <div class="card">
-          <h2 class="name">${data.name}</h2>
-          <img class="image"src="${data.Image}" alt="">
-          <h3 class="price">${data.cost}</h3>
-          <a href="${data.link}"> <button>Click Here to Visit Site</button> </a>
-          </div>
-          `
-    );
-  });
-});
-dom.instockbtn.addEventListener("click", function () {
-  dom.display.innerHTML = "";
-  data
-    .filter((data) => data.instock === true)
-    .forEach((data) => {
-      dom.display.insertAdjacentHTML(
-        "afterbegin",
-        ` 
-      <div class="card">
+}
+function insertcard() {
+  dom.display.insertAdjacentHTML(
+    "afterbegin",
+    ` 
+        <div class="card">
             <h2 class="name">${data.name}</h2>
-            <img class="image"src="${data.Image}" alt="">
-            <h3 class="price">${data.cost}</h3>
-            <a href="${data.link}"> <button>Click Here to Visit Site</button> </a>
-            </div>
-            `
-      );
-    });
-});
-dom.animalbtn.addEventListener("click", function () {
-  dom.display.innerHTML = "";
-  data
-    .filter((data) => data.animalrelated === true)
-    .forEach((data) => {
-      dom.display.insertAdjacentHTML(
-        "afterbegin",
-        ` 
-      <div class="card">
-            <h2 class="name">${data.name}</h2>
-            <img class="image"src="${data.Image}" alt="">
-            <h3 class="price">${data.cost}</h3>
-            <a href="${data.link}"> <button>Click Here to Visit Site</button> </a>
-            </div>
-            `
-      );
-    });
-});
-dom.theme.addEventListener("click", function () {
-  if (theme) {
-  }
+             <img class="image"src="${data.Image}" alt="">
+              <h3 class="price">${data.cost}</h3>
+              <a href="${data.link}"> <button>Click Here to Visit Site</button> </a>
+              </div>
+            
+              `
+  );
+}
+const all = data.forEach((data) => {
+  insertcard(data);
 });
